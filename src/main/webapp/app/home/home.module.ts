ad0c4853af725d60b832282ad9d7f9c8f5390de3
alex_jd <twoalexan@mail.ru>
2017-10-15 22:29:05 +0500

    Add the side-bar.module.ts, import it at home.module.ts and transfer the part of home.component.html into the side-bar.component.html

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -7,11 +7,13 @@ import { HOME_ROUTE, HomeComponent } from './';
 import { TagComponent } from '../entities/tag/tag.component';
 import { FrontPostComponent } from '../entities/post/front/front-post.component';
 import { SideBarComponent } from '../home/side-bar/side-bar.component';
+import {JavaBlogSideBarModule} from "./side-bar/side-bar.module";
 
 @NgModule({
     imports: [
         JavablogSharedModule,
-        RouterModule.forRoot([ HOME_ROUTE ], { useHash: true })
+        RouterModule.forRoot([ HOME_ROUTE ], { useHash: true }),
+        JavaBlogSideBarModule,
     ],
     declarations: [
         HomeComponent,
